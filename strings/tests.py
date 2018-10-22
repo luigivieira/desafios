153616dcaf0eb77491312d28fc17c77aea7e8396
Luiz Carlos Vieira <luigivieira@gmail.com>
2018-10-21 21:45:24 -0300

    Added extra tests using lorem ipsum text.

--- strings/tests.py
+++ strings/tests.py
@@ -19,12 +19,36 @@ class TestStringChallenge(unittest.TestCase):
         with open('output-parte2.txt', mode='r', encoding='utf-8') as file:
             self.output_part2 = file.read()
 
+        with open('lorem_input.txt', mode='r', encoding='utf-8') as file:
+            self.lorem = file.read()
+
+        with open('lorem_output1.txt', mode='r', encoding='utf-8') as file:
+            self.lorem_output1 = file.read()
+
+        with open('lorem_output2.txt', mode='r', encoding='utf-8') as file:
+            self.lorem_output2 = file.read()
+
+        with open('lorem_output3.txt', mode='r', encoding='utf-8') as file:
+            self.lorem_output3 = file.read()
+
     # --------------------------------------------------------------------------
     def test_empty_string(self):        
         '''Tests that empty strings are unchanged.'''
 
         output = word_wrap_text('')
         self.assertEqual(output, '')
+
+    # --------------------------------------------------------------------------
+    def test_many_calls(self):
+        '''Tests that the code work for any max line length between 0 and 100.'''
+        n = 100
+        for i in range(n):
+            with self.subTest(i):
+                output = word_wrap_text(self.input, max_line_len=i, justify=False)
+                self.assertGreater(len(output), 0)
+            with self.subTest(n + i):
+                output = word_wrap_text(self.input, max_line_len=i, justify=False)
+                self.assertGreater(len(output), 0)
 
     # --------------------------------------------------------------------------
     def test_input_sample_part1(self):
@@ -39,6 +63,35 @@ class TestStringChallenge(unittest.TestCase):
 
         output = word_wrap_text(self.input, justify=True)
         self.assertEqual(output, self.output_part2)
+
+    # --------------------------------------------------------------------------
+    def test_lorem_sample_part1(self):
+        '''Tests that the part 1 of the lorem-ipsum sample performs correctly.'''
+
+        output = word_wrap_text(self.lorem, max_line_len=80, justify=False)
+        self.assertEqual(output, self.lorem_output1)
+
+    # --------------------------------------------------------------------------
+    def test_lorem_sample_part2(self):
+        '''Tests that the part 2 of the lorem-ipsum sample performs correctly.'''
+
+        output = word_wrap_text(self.lorem, max_line_len=80, justify=True)
+        self.assertEqual(output, self.lorem_output2)
+
+    # --------------------------------------------------------------------------
+    def test_lorem_sample_part3(self):
+        '''Tests that the part 3 of the lorem-ipsum sample performs correctly.
+        That means, test if using values of max line length equal to 0 or 1
+        will always break the text to have one word per line.
+        '''
+
+        for i in range(2):
+            with self.subTest(i):
+                output = word_wrap_text(self.lorem, max_line_len=i, justify=False)
+                self.assertEqual(output, self.lorem_output3)
+            with self.subTest(2+i):
+                output = word_wrap_text(self.lorem, max_line_len=i, justify=True)
+                self.assertEqual(output, self.lorem_output3)
 
 # ==============================================================================
 if __name__ == '__main__':
